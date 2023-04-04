2e0ce5e26294e9e990aaf994d3e38b716b0c64be
recoilme <vadim-kulibaba@yandex.ru>
2023-04-04 10:41:03 +0300

    merge by unet layers

--- weightedsim.py
+++ weightedsim.py
@@ -11,8 +11,7 @@ parser.add_argument("--out", type=str, help="Output file name, without extension
 parser.add_argument("--device", type=str, help="Device to use, defaults to cpu", default="cpu", required=False)
 parser.add_argument("--without_vae", action="store_false", help="Do not merge VAE", required=False)
 parser.add_argument("--dry", action="store_true", help="dry_run", default=False, required=False)
-parser.add_argument("--soft", action="store_true", help="soft", default=False, required=False)
-parser.add_argument("--s", type=float, help="share a b", default=.5, required=False)
+parser.add_argument("--s", type=float, help="share of model a", default=.5, required=False)
 args = parser.parse_args()
 
 
@@ -25,44 +24,36 @@ def loadModelWeights(mPath):
 
 output_file = f'{args.out}-{args.s}.ckpt'
 
-step = 0
 a, b = loadModelWeights(args.a), loadModelWeights(args.b)
 sim = torch.nn.CosineSimilarity(dim=0)
-sims = np.array([], dtype=np.float64)
 
-for key in tqdm(a.keys(), desc="Stage 1/3"):
-    # skip VAE model parameters to get better results
-    if args.without_vae and "first_stage_model" in key: continue
-    if "model" in key and key in b:
-        simab = sim(a[key].to(torch.float64), b[key].to(torch.float64))
-        sims = np.append(sims,simab.numpy())
-sims = sims[~np.isnan(sims)]
-#print(len(sims),sims.min(),sims.max())
-sims = np.delete(sims, np.where(sims<np.percentile(sims, 1 ,method = 'midpoint')))
-sims = np.delete(sims, np.where(sims>np.percentile(sims, 99 ,method = 'midpoint')))
-print(len(sims),sims.min(),sims.max())
-#sims = np.delete(sims, np.where(sims<sims.mean() - 3 * sims.std()))
-#sims = np.delete(sims, np.where(sims>sims.mean() + 3 * sims.std()))
-#print(len(sims),sims.min(),sims.max())
-
-
-i = -1
-for key in tqdm(a.keys(), desc="Stage 2/3"):
-    # skip VAE model parameters to get better results
-    if args.without_vae and "first_stage_model" in key: continue
-    if "model" in key and key in b:
-        i+=1
-        simab = sim(a[key].to(torch.float32), b[key].to(torch.float32))
-        k = (simab - sims.min())/(sims.max() - sims.min())
-        k = k - args.s
-        k = k.clip(min=.0,max=1.)
-        if args.soft:
-            a[key] = a[key] * (1 - k) + b[key] * k
-        else:
-            a[key] = a[key] * k + b[key] * (1 - k)
+for key in tqdm(a.keys(), desc="Stage 1/1"):
+    # prune vae
+    if "first_stage_model" in key:
         a[key] = a[key].to(torch.float16)
-for key in tqdm(b.keys(), desc="Stage 3/3"):
-	if "model" in key and key not in a: a[key] = b[key]
+        continue
+    # not in b
+    if not key in b:
+        continue
+    # merge not mergeble (2.0 with 1.5)?
+    if (a[key].size() != b[key].size()):
+        continue
+    # unet merge
+    if "model.diffusion_model" in key and key in b:
+        sims = np.array([], dtype=np.float32)
+        simab = sim(a[key].to(torch.float32), b[key].to(torch.float32))
+        sims = np.append(sims,simab.detach().numpy())
+        sims = sims[~np.isnan(sims)]
+        sims = np.delete(sims, np.where(sims<np.percentile(sims, 1 ,method = 'midpoint')))
+        sims = np.delete(sims, np.where(sims>np.percentile(sims, 99 ,method = 'midpoint')))
+        if len(sims)!=1 and sims.min()!=sims.max():
+            k = (simab - sims.min())/(sims.max() - sims.min())
+            k = k - args.s
+            k = k.clip(min=.0,max=1.)
+            a[key] = a[key] * k + b[key] * (1 - k)
+            a[key] = a[key].to(torch.float16)
+            if args.dry == 1:
+                print(len(sims),sims.min(),sims.max(),key)
 
 if args.dry == 0:
     print("Saving...")
